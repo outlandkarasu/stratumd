e8920974ec695e4edb8370faa38aa9b9128075ce
outland.karasu <outland.karasu@gmail.com>
2021-04-16 23:52:13 +0900

    implement methods.

--- source/stratumd/methods.d
+++ source/stratumd/methods.d
@@ -1,6 +1,7 @@
 module stratumd.methods;
 
 import std.json : JSONValue, toJSON;
+import std.typecons : Nullable, nullable;
 
 /**
 Stratum authorize method.
@@ -8,23 +9,11 @@ Stratum authorize method.
 struct StratumAuthorize
 {
     enum method = "mining.authorize";
+    alias Result = StratumBooleanResult;
 
     int id;
     string username;
     string password;
-
-    struct Result
-    {
-        int id;
-        bool result;
-
-        static Result parse()(auto ref const(JSONValue) json)
-        {
-            immutable id = cast(int) json["id"].integer;
-            immutable result = json["result"].boolean;
-            return Result(id, result);
-        }
-    }
 
     string toJSON() const
     {
@@ -41,17 +30,6 @@ unittest
     assert(authorize.toJSON ==
         `{"id":1,"method":"mining.authorize",`
         ~ `"params":["testname","testword"]}`);
-}
-
-///
-unittest
-{
-    import std.json : parseJSON;
-
-    auto json = parseJSON(`{"id":1,"error":null,"result":true}`);
-    immutable result = StratumAuthorize.Result.parse(json);
-    assert(result.id == 1);
-    assert(result.result == true);
 }
 
 /**
@@ -113,19 +91,32 @@ Stratum submit method.
 */
 struct StratumSubmit
 {
+    enum method = "mining.submit";
+    alias Result = StratumBooleanResult;
+
+    int id;
     string workerName;
     string jobID;
     string extraNonce2;
     string ntime;
     string nonce;
+
+    string toJSON() const
+    {
+        auto json = createMethodJSON(id, method);
+        json["params"] = [workerName, jobID, extraNonce2, ntime, nonce];
+        return json.toJSON();
+    }
 }
 
-/**
-Stratum submit result.
-*/
-struct StratumSubmitResult
+///
+unittest
 {
-    bool result;
+    auto authorize = StratumSubmit(
+        1, "test-worker", "test-job", "exnonce2", "abcdef", "ghqr");
+    assert(authorize.toJSON ==
+        `{"id":1,"method":"mining.submit",`
+        ~ `"params":["test-worker","test-job","exnonce2","abcdef","ghqr"]}`);
 }
 
 /**
@@ -133,15 +124,56 @@ Stratum notify method.
 */
 struct StratumNotify
 {
+    enum method = "mining.notify";
+
     string jobID;
     string prevHash;
     string coinb1;
     string coinb2;
-    shared string[] merkleBranch;
+    string merkleBranch1;
+    string merkleBranch2;
     string blockVersion;
     string nbits;
     string ntime;
     bool cleanJobs;
+
+    static StratumNotify parse()(const(JSONValue)[] params)
+    {
+        auto markleBranches = params[4].array;
+        return StratumNotify(
+            params[0].str,
+            params[1].str,
+            params[2].str,
+            params[3].str,
+            markleBranches[0].str,
+            markleBranches[1].str,
+            params[5].str,
+            params[6].str,
+            params[7].str,
+            params[8].boolean);
+    }
+}
+
+///
+unittest
+{
+    import std.json : parseJSON;
+
+    auto json = parseJSON(
+        `{"id":1,"method":"mining.notify",`
+        ~ `"params":["job-id","prev-hash","coinb1","coinb2",`
+        ~ `["mb1","mb2"],"bversion","1234","5678",true]}`);
+    immutable result = StratumNotify.parse(json["params"].array);
+    assert(result.jobID == "job-id");
+    assert(result.prevHash == "prev-hash");
+    assert(result.coinb1 == "coinb1");
+    assert(result.coinb2 == "coinb2");
+    assert(result.merkleBranch1 == "mb1");
+    assert(result.merkleBranch2 == "mb2");
+    assert(result.blockVersion == "bversion");
+    assert(result.nbits == "1234");
+    assert(result.ntime == "5678");
+    assert(result.cleanJobs == true);
 }
 
 /**
@@ -149,7 +181,24 @@ Stratum set_difficulty method.
 */
 struct StratumSetDifficulty
 {
+    enum method = "mining.set_difficulty";
+
     int difficulty;
+
+    static StratumSetDifficulty parse()(const(JSONValue)[] params)
+    {
+        return StratumSetDifficulty(cast(int) params[0].integer);
+    }
+}
+
+///
+unittest
+{
+    import std.json : parseJSON;
+
+    auto json = parseJSON(`{"id":1,"params":[4]}`);
+    immutable result = StratumSetDifficulty.parse(json["params"].array);
+    assert(result.difficulty == 4);
 }
 
 /**
@@ -157,8 +206,26 @@ Stratum set extranonce.
 */
 struct StratumSetExtranonce
 {
+    enum method = "mining.set_extranonce";
+
     string extranonce1;
     int extranonce2Size;
+
+    static StratumSetExtranonce parse()(const(JSONValue)[] params)
+    {
+        return StratumSetExtranonce(params[0].str, cast(int) params[1].integer);
+    }
+}
+
+///
+unittest
+{
+    import std.json : parseJSON;
+
+    auto json = parseJSON(`{"id":1,"params":["nonce1",8]}`);
+    immutable result = StratumSetExtranonce.parse(json["params"].array);
+    assert(result.extranonce1 == "nonce1");
+    assert(result.extranonce2Size == 8);
 }
 
 /**
@@ -166,13 +233,41 @@ Stratum reconnect.
 */
 struct StratumReconnect
 {
+    enum method = "client.reconnect";
+
+    struct Result
+    {
+        int id;
+    }
+
+    static StratumRecoonect parse()(const(JSONValue)[] params)
+    {
+        return StratumReconnect.init;
+    }
 }
 
-/**
-Stratum reconnect result.
-*/
-struct StratumReconnectResult
+struct StratumBooleanResult
 {
+    int id;
+    bool result;
+
+    static StratumBooleanResult parse()(auto ref const(JSONValue) json)
+    {
+        immutable id = cast(int) json["id"].integer;
+        immutable result = json["result"].boolean;
+        return StratumBooleanResult(id, result);
+    }
+}
+
+///
+unittest
+{
+    import std.json : parseJSON;
+
+    auto json = parseJSON(`{"id":1,"error":null,"result":true}`);
+    immutable result = StratumBooleanResult.parse(json);
+    assert(result.id == 1);
+    assert(result.result == true);
 }
 
 private:
