a006bcabc263063d7641e5813b416d91067a4ab0
outland.karasu <outland.karasu@gmail.com>
2021-05-13 23:27:32 +0900

    implement client.

--- source/stratumd/client.d
+++ source/stratumd/client.d
@@ -1,14 +1,16 @@
 module stratumd.client;
 
+import core.time : seconds, msecs, Duration;
 import std.experimental.logger : tracef, errorf;
 import std.concurrency :
     thisTid,
     Tid,
     spawnLinked,
     send,
-    OwnerTerminated;
-import std.exception :
-    basicExceptionCtors;
+    OwnerTerminated,
+    receiveTimeout;
+import std.exception : basicExceptionCtors;
+import std.typecons : Nullable, nullable;
 
 import stratumd.rpc_connection : MessageID;
 import stratumd.stratum_connection : 
@@ -32,6 +34,14 @@ Stratum client.
 final class StratumClient
 {
     /**
+    Close if connected.
+    */
+    ~this()
+    {
+        close();
+    }
+
+    /**
     Connect to host.
     */
     void connect(string hostname, ushort port)
@@ -41,15 +51,89 @@ final class StratumClient
             throw new StratumClientException("Already connected");
         }
 
-        connectionTid_ = spawnLinked(&connectionThread, hostname, port);
+        connectionTid_ = spawnLinked(&connectionThread, hostname, port, thisTid);
+        if (!receiveTimeout(responseTimeout, (Connected r) {}))
+        {
+            close();
+            throw new StratumClientException("connection timed out");
+        }
+    }
+
+    /**
+    Authorize worker.
+    */
+    void authorize(string username, string password)
+    {
+        connectionTid_.send(Authorizing(username, password));
+        waitResponse!Authorized();
+    }
+
+    /**
+    Subscribe jobs.
+    */
+    void subscribe(string userAgent)
+    {
+        connectionTid_.send(Subscribing(userAgent));
+        waitResponse!Subscribed();
+    }
+
+    /**
+    Wait new job.
+    */
+    Nullable!Job waitNewJob()
+    {
+        typeof(return) job;
+        receiveTimeout(responseTimeout, (JobNotify notify) { job = notify.job; });
+        return job;
+    }
+
+    /**
+    Submit job result.
+    */
+    void submit(JobResult jobResult)
+    {
+        connectionTid_.send(Submitting(jobResult));
+        waitResponse!Submitted();
+    }
+
+    /**
+    Close connection.
+    */
+    void close()
+    {
+        if (connectionTid_ != Tid.init)
+        {
+            connectionTid_.send(Closing());
+            connectionTid_ = Tid.init;
+        }
     }
 
 private:
 
+    static immutable Duration requestWaitTimeout = 10.msecs;
+    static immutable Duration responseTimeout = 60.seconds;
+
+    struct Closing {}
     struct Connected {}
+
+    struct Authorizing
+    {
+        string username;
+        string password;
+    }
+
     struct Authorized { bool result; }
+
+    struct Subscribing { string userAgent; }
     struct Subscribed { bool result; }
+
+    struct Submitting
+    {
+        JobResult jobResult;
+    }
+
     struct Submitted { bool result; }
+
     struct JobNotify { Job job; }
 
     Tid connectionTid_;
@@ -79,6 +163,8 @@ private:
                 connected_ = true;
                 sendAndCheck(Connected(), sender);
             }
+
+            while (waitRequest(sender)) {}
         }
 
         override void onResponse(MessageID id, StratumMethod method, scope StratumSender sender)
@@ -127,11 +213,44 @@ private:
                 sender.close();
             }
         }
+
+        bool waitRequest(scope StratumSender sender)
+        {
+            return receiveTimeout(
+                requestWaitTimeout,
+                (Authorizing request)
+                {
+                    sender.authorize(request.username, request.password);
+                },
+                (Submitting request)
+                {
+                    sender.submit(request.jobResult);
+                },
+                (Subscribing request)
+                {
+                    sender.subscribe(request.userAgent);
+                });
+        }
     }
 
-    static void connectionThread(string hostname, ushort port)
+    static void connectionThread(string hostname, ushort port, Tid parentTid)
     {
-        openStratumConnection(hostname, port, null);
+        scope handler = new Handler(parentTid);
+        openStratumConnection(hostname, port, handler);
+    }
+
+    void waitResponse(R)()
+    {
+        bool result = false;
+        if (!receiveTimeout(responseTimeout, (R r) { result = r.result; }))
+        {
+            throw new StratumClientException("response timed out");
+        }
+
+        if (!result)
+        {
+            throw new StratumClientException("request error");
+        }
     }
 }
 
